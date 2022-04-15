dfd4e13328633f2b582a5bc30d18c60cbb671f12
Nicholas Wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-04-15 14:40:22 -0400

    Updated Project urls.py to includesuper_types

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from . serializers import SupersSerializer
+from . models import 
 
 # Create your views here.
+
+
