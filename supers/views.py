5e4d5032d7a8a44270ce54ced6f2fe45e81476fc
Nicholas Wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-04-18 18:05:45 -0400

    Updated supers_table function in supers App for custom response

--- supers/views.py
+++ supers/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from . serializers import SupersSerializer
 from . models import Supers
+from super_types.models import SuperType
 
 
 # Create your views here.
@@ -11,22 +12,26 @@ from . models import Supers
 @api_view(['GET','POST'])
 def supers_table(request):
     
-    
     if request.method == 'GET':
         super_type_param = request.query_params.get('type')
         sort_param = request.query_params.get('sort')
-
         supers = Supers.objects.all()
-
         if super_type_param:
             supers = supers.filter(super_type__type=super_type_param)
-
         if sort_param:
             supers = supers.order_by(sort_param)
-
-
         serializer = SupersSerializer(supers, many=True)
         return Response(serializer.data)
+
+    elif request.method == 'GET':
+        supers_types = Supers.objects.all()
+        Custom_response = {}
+        for super_type in supers_types:
+            supers = Supers.objects.filter(super_type__type=super_type.type)
+            serializer = SupersSerializer(supers, many=True)
+            Custom_response[super_type.type] = {"Heroes": [], "Villains": []}
+
+        return Response(Custom_response)
 
 
     elif request.method == 'POST':
